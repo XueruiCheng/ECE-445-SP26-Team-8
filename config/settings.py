98d3a471596ed2688e3d8dcaea859c8c08b8933a
Ethan Zhang <ezhang3333@gmail.com>
2026-03-18 13:49:54 -0400

    Parsed html from quantum zeitgeist

--- config/settings.py
+++ config/settings.py
@@ -3,3 +3,5 @@ import os
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
 CATEGORIES = ["scientists", "engineers", "entrepreneurs"]
+
+QUANTUM_SCIENTIST_DATABASE_URL = "https://quantumzeitgeist.com/influential-people-in-quantum-computing/"
